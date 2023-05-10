8b8c2c814c421a852f8596c0b6cdcd3aec6082f2
Daisuke Sakamoto <daisuke_sakamoto@pressman.ne.jp>
2023-05-10 15:00:48 +0900

    Classic EditorとCustom Post Type UIプラグインのインストール

---
+++ wp-content/plugins/curriculum01/curriculum01.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name:[福利厚生]購入書籍管理プラグイン
+Plugin URI:https://pressman.ne.jp
+Description: 福利厚生で購入した書籍を管理するプラグインです。
+Version: 1.0
+Author: 阪本大将 (Daisuke Sakamoto)
+Author URI: https://sakamoto.pressman.ne.jp
+License: GPL2
+*/
+
+// 各種情報定義
+define('CC01_PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+require_once CC01_PLUGIN_DIR . '/class.cc01.php';
+
+Curriculum_01::get_instance();
+
+
+
+

52b8e5671acd26fe1628fbb41452e949d8a9873c
Daisuke Sakamoto <daisuke_sakamoto@pressman.ne.jp>
2023-05-10 15:02:03 +0900

    独自プラグイン（ACF半角英数バリデーション）

--- wp-content/plugins/curriculum01/class.cc01.php
+++ wp-content/plugins/curriculum01/class.cc01.php
@@ -12,97 +12,28 @@ class Curriculum_01 {
     }
 
     public function __construct() {
-        // メニュー追加
-        add_action( 'admin_menu', array( $this, 'add_book_menu_page' ), 10 );
-        add_action( 'admin_menu', array( $this, 'add_comment_menu_page' ), 10 );
+        add_filter('acf/validate_value/name=book_number', [$this, 'only_alphanumeric'], 10, 4);
     }
 
-    // View
-    public static function view( $name) {
-        $file = CC01_PLUGIN_DIR . 'views/'. $name . '.php';
-        include($file);
-    }
-
-    // メニュー
-    public function add_book_menu_page() {
-        add_menu_page(
-            __( '福利厚生書籍', '' ),                // ページのタイトルタグ<title>に表示されるテキスト
-            __( "福利厚生書籍", '' ),                // 左メニューとして表示されるテキスト
-            'manage_options',               // 必要な権限 manage_options は通常 administrator のみに与えられた権限
-            'welfare-books',               // 左メニューのスラッグ名 →URLのパラメータに使われる /wp-admin/admin.php?page=toriaezu_menu
-            array( $this, 'top_page' ),     // メニューページを表示する際に実行される関数
-            'dashicons-book',        // メニューのアイコンを指定 https://developer.wordpress.org/resource/dashicons/#awards
-            99                               // メニューが表示される位置のインデックス(0が先頭) 5=投稿,10=メディア,20=固定ページ,25=コメント,60=テーマ,65=プラグイン,70=ユーザー,75=ツール,80=設定
-        );
-
-        add_submenu_page(
-            'welfare-books'    // 親メニューのスラッグ
-            , '購入書籍一覧' // ページのタイトルタグ<title>に表示されるテキスト
-            , '購入書籍一覧' // サブメニューとして表示されるテキスト
-            , 'manage_options' // 必要な権限 manage_options は通常 administrator のみに与えられた権限
-            , 'book-list'  // サブメニューのスラッグ名。この名前を親メニューのスラッグと同じにすると親メニューを押したときにこのサブメニューを表示します。一般的にはこの形式を採用していることが多い。
-            , array($this, 'book_list') //（任意）このページのコンテンツを出力するために呼び出される関数
-        );
-
-        add_submenu_page(
-            'welfare-books'    // 親メニューのスラッグ
-            , '購入書籍の登録' // ページのタイトルタグ<title>に表示されるテキスト
-            , '購入書籍の登録' // サブメニューとして表示されるテキスト
-            , 'manage_options' // 必要な権限 manage_options は通常 administrator のみに与えられた権限
-            , 'book-add'  // サブメニューのスラッグ名。この名前を親メニューのスラッグと同じにすると親メニューを押したときにこのサブメニューを表示します。一般的にはこの形式を採用していることが多い。
-            , array($this, 'view_add_book') //（任意）このページのコンテンツを出力するために呼び出される関数
-        );
-
-    }
-
-    public function add_comment_menu_page()
+    /**
+     * 半角英数のみ許可 (ACF)
+     * @param $valid
+     * @param $value
+     * @param $field
+     * @param $input_name
+     * @return mixed|string|true|null
+     */
+    public function only_alphanumeric($valid, $value, $field, $input_name): mixed
     {
-        add_menu_page(
-            __( '購読書籍の感想', '' ),                // ページのタイトルタグ<title>に表示されるテキスト
-            __( '購読書籍の感想', '' ),                // 左メニューとして表示されるテキスト
-            'manage_options',               // 必要な権限 manage_options は通常 administrator のみに与えられた権限
-            'welfare-book-comment',               // 左メニューのスラッグ名 →URLのパラメータに使われる /wp-admin/admin.php?page=toriaezu_menu
-            array( $this, 'view_sample_page' ),     // メニューページを表示する際に実行される関数
-            'dashicons-admin-comments',        // メニューのアイコンを指定 https://developer.wordpress.org/resource/dashicons/#awards
-            99                               // メニューが表示される位置のインデックス(0が先頭) 5=投稿,10=メディア,20=固定ページ,25=コメント,60=テーマ,65=プラグイン,70=ユーザー,75=ツール,80=設定
-        );
+        if( $valid !== true ) {
+            return $valid;
+        }
 
-        add_submenu_page(
-            'welfare-book-comment'    // 親メニューのスラッグ
-            , '感想一覧' // ページのタイトルタグ<title>に表示されるテキスト
-            , '感想一覧' // サブメニューとして表示されるテキスト
-            , 'manage_options' // 必要な権限 manage_options は通常 administrator のみに与えられた権限
-            , 'book-comment-list'  // サブメニューのスラッグ名。この名前を親メニューのスラッグと同じにすると親メニューを押したときにこのサブメニューを表示します。一般的にはこの形式を採用していることが多い。
-            , array($this, 'view_sample_page') //（任意）このページのコンテンツを出力するために呼び出される関数
-        );
+        if (!preg_match("/^[a-zA-Z0-9]+$/", $value)) {
+            return __('半角英数字で入力してください。');
+        }
 
-        add_submenu_page(
-            'welfare-book-comment'    // 親メニューのスラッグ
-            , '感想の登録' // ページのタイトルタグ<title>に表示されるテキスト
-            , '感想の登録' // サブメニューとして表示されるテキスト
-            , 'manage_options' // 必要な権限 manage_options は通常 administrator のみに与えられた権限
-            , 'book-comment-add'  // サブメニューのスラッグ名。この名前を親メニューのスラッグと同じにすると親メニューを押したときにこのサブメニューを表示します。一般的にはこの形式を採用していることが多い。
-            , array($this, 'view_add_comment') //（任意）このページのコンテンツを出力するために呼び出される関数
-        );
-    }
-
-    public function top_page() {
-        $this->view('index');
-    }
-
-    public function book_list()
-    {
-        $this->view('index');
-    }
-
-    public function view_add_book()
-    {
-        $this->view('add_book');
-    }
-
-    public function view_add_comment()
-    {
-        $this->view('add_comment');
+        return $valid;
     }
 
 }
--- wp-content/plugins/curriculum01/curriculum01.php
+++ wp-content/plugins/curriculum01/curriculum01.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name:[福利厚生]購入書籍管理プラグイン
 Plugin URI:https://pressman.ne.jp
-Description: 福利厚生で購入した書籍を管理するプラグインです。
+Description: 福利厚生で購入した書籍を管理するプラグインです。特定のACFフィールドのバリデーションを設定します。
 Version: 1.0
 Author: 阪本大将 (Daisuke Sakamoto)
-Author URI: https://sakamoto.pressman.ne.jp
+Author URI: https://pressman.ne.jp
 License: GPL2
 */
 
@@ -15,7 +15,6 @@ define('CC01_PLUGIN_DIR', plugin_dir_path(__FILE__));
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
-
 
 require_once CC01_PLUGIN_DIR . '/class.cc01.php';
 
